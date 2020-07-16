c28dceff8e3068fc6fe7cde12705d368ebb45aed
Valdimir Indik <vladimir.indik@revolut.com>
2020-07-16 20:59:47 +0200

    Add more tests and first solution for power-hungry

--- power-hungry/solution.py
+++ power-hungry/solution.py
@@ -1,2 +1,23 @@
 def solution(xs):
-    return 8
+    min_negative = 0
+    nonzeros = 0
+    result = 0
+
+    for value in xs:
+        if value == 0:
+            continue
+        nonzeros += 1
+        if result == 0:
+            result = 1
+        result *= value
+
+        if value < 0:
+            if min_negative < value or min_negative == 0:
+                min_negative = value
+
+    if result < 0:
+        if nonzeros < 2:
+            return 0
+        result /= min_negative
+
+    return result
--- power-hungry/solution_test.py
+++ power-hungry/solution_test.py
@@ -4,11 +4,47 @@ import unittest
 import solution
 
 class WidgetTestCase(unittest.TestCase):
+    def test_google0(self):
+        self.assertEqual(solution.solution([2, -3, 1, 0, -5]), 30)
+
     def test_google1(self):
         self.assertEqual(solution.solution([2, 0, 2, 2, 0]), 8)
 
     def test_google2(self):
         self.assertEqual(solution.solution([-2, -3, 4, -5]), 60)
+
+    def test_one_positive(self):
+        self.assertEqual(solution.solution([2]), 2)
+
+    def test_one_negative(self):
+        self.assertEqual(solution.solution([-2]), 0)
+
+    def test_zero(self):
+        self.assertEqual(solution.solution([0]), 0)
+
+    def test_zeros(self):
+        self.assertEqual(solution.solution([0, 0, 0, 0]), 0)
+
+    def test_one(self):
+        self.assertEqual(solution.solution([1]), 1)
+
+    def test_ones(self):
+        self.assertEqual(solution.solution([1, 1, 1]), 1)
+
+    def test_minus_ones(self):
+        self.assertEqual(solution.solution([-1, -1, -1]), 1)
+
+    def test_various_ones(self):
+        self.assertEqual(solution.solution([1, -1]), 1)
+
+    def test_one_negative_one_positive(self):
+        self.assertEqual(solution.solution([1, -2]), 1)
+
+    def test_one_negative(self):
+        self.assertEqual(solution.solution([-2]), 0)
+
+    def test_one_negative_zero(self):
+        self.assertEqual(solution.solution([-2, 0]), 0)
 
 if __name__ == '__main__':
     unittest.main()
