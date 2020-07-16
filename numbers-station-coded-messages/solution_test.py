055c39fbe6a4f6c7cf3b4d7247d15b8b2f391bd7
Valdimir Indik <vladimir.indik@revolut.com>
2020-07-16 19:38:37 +0200

    Add more tests for numbers-station-coded-messages

--- numbers-station-coded-messages/solution_test.py
+++ numbers-station-coded-messages/solution_test.py
@@ -10,11 +10,32 @@ class WidgetTestCase(unittest.TestCase):
     def test_google2(self):
         self.assertEqual(solution.solution([4, 3, 10, 2, 8], 12), [2, 3])
 
-    def test_single1(self):
+    def test_single(self):
         self.assertEqual(solution.solution([4, 3, 12, 2, 8], 12), [2, 2])
 
     def test_single2(self):
         self.assertEqual(solution.solution([4], 4), [0, 0])
+
+    def test_single2(self):
+        self.assertEqual(solution.solution([4, 13, 12, 2, 8], 12), [2, 2])
+
+    def test_single3(self):
+        self.assertEqual(solution.solution([13], 12), [-1, -1])
+
+    def test_single4(self):
+        self.assertEqual(solution.solution([4, 13, 12, 2, 8], 12), [2, 2])
+
+    def test_single5(self):
+        self.assertEqual(solution.solution([12], 13), [-1, -1])
+
+    def test_single6(self):
+        self.assertEqual(solution.solution([4, 13, 12, 2, 8], 8), [4, 4])
+
+    def test_single7(self):
+        self.assertEqual(solution.solution([4, 13, 12, 2, 8], 4), [0, 0])
+
+    def test_single7(self):
+        self.assertEqual(solution.solution([43, 13, 12, 2, 8], 8), [4, 4])
 
 if __name__ == '__main__':
     unittest.main()
