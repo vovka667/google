71e312b355041eeb38378c15ce4eff36f96235b0
Valdimir Indik <vladimir.indik@revolut.com>
2020-07-23 11:42:26 +0200

    Add naive solution for find-the-access-codes

--- find-the-access-codes/solution.py
+++ find-the-access-codes/solution.py
@@ -1,2 +1,15 @@
 def solution(l):
-    return 3
+    result = 0
+
+    for i in range(len(l)):
+        for j in range(i+1, len(l)):
+            if l[j] % l[i] != 0:
+                continue
+
+            for k in range(j+1, len(l)):
+                if l[k] % l[j] != 0:
+                    continue
+
+                result += 1
+
+    return result
--- find-the-access-codes/solution_test.py
+++ find-the-access-codes/solution_test.py
@@ -10,5 +10,14 @@ class WidgetTestCase(unittest.TestCase):
     def test_google1(self):
         self.assertEqual(solution.solution([1, 1, 1]), 1)
 
+    def test_mine0(self):
+        self.assertEqual(solution.solution([1]), 0)
+
+    def test_mine1(self):
+        self.assertEqual(solution.solution([1, 1]), 0)
+
+    def test_mine2(self):
+        self.assertEqual(solution.solution([1, 1, 1, 1]), 4)
+
 if __name__ == '__main__':
     unittest.main()
