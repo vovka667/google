bd8ecab9f1c3c8846c8ab60014b0fa48ac4c108c
Valdimir Indik <vladimir.indik@revolut.com>
2020-08-12 13:31:46 +0200

    Add solution for bomb-baby

--- bomb-baby/solution.py
+++ bomb-baby/solution.py
@@ -1,2 +1,23 @@
 def solution(m, f):
-    return "1"
+    generations = long(0)
+    m = long(m)
+    f = long(f)
+    while True:
+        if m == 0 or f == 0:
+            return 'impossible'
+
+        if m == 1:
+            generations += f - m
+            break
+        elif f == 1:
+            generations += m - f
+            break
+
+        if m > f:
+            generations += m // f
+            m %= f
+        elif f > m:
+            generations += f // m
+            f %= m
+
+    return str(generations)
